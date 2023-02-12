f740c09b199a2279f296e019364ee5f44f4dc70b
JuaniTopo <juana.der@gmail.com>
2023-02-12 16:32:22 -0300

    Agregar nuevas urls al proyecto

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -3,9 +3,17 @@ from django.http import HttpResponse
 from AppCoder.models import Curso
 # Create your views here.
 
+def inicio(request):
+    return HttpResponse("vista inicio")
 
-def curso(self):
-    curso = Curso(nombre="Desarrollo Web", comision=19889)
-    curso.save()
-    documentoDeTexto= f"--->Curso:{curso.nombre} comision: {curso.comision}"
-    return HttpResponse(documentoDeTexto)
+def curso(request):
+    return HttpResponse("vista cursos")
+
+def profesores(request):
+    return HttpResponse("vista profesores")
+
+def entregables(request):
+    return HttpResponse("vista entregables")
+
+def estudiantes(request):
+    return HttpResponse("vista estudiantes")
